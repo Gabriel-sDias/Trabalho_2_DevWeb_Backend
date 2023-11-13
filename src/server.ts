40f210158b68a2b55257d6021d9ce31d18f644cd
Gabriel Silveira <gabrielsd@unifei.edu.br>
2023-11-13 03:19:20 -0300

    configando ambiente para vercel

--- src/server.ts
+++ src/server.ts
@@ -4,11 +4,11 @@ import express from "express";
 
 import cors from "cors";
 import createAplicationRouter from "./routes/routes";
-
+const port = process.env.PORT || 5555;
 const app = express();
 
 app.use(express.json());
 app.use(cors({ origin: "*" }));
 app.use(createAplicationRouter());
 
-app.listen(5555, () => console.log("server up"));
+app.listen(port, () => console.log("server up"));
